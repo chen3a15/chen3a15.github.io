7604e4627ddab5860fd3b325cea8e2b84f0cc939
chen3a15 <chen3a15@163.com>
2023-10-31 16:21:59 +0800

    收藏夹

---
+++ 工具/收藏夹/data.js
@@ -0,0 +1,8 @@
+data = `
+{"title": "山东省机动车驾驶培训监管服务平台", "link": "http://103.239.155.248:3700/"}
+{"title": "公安部互联网交通安全综合服务管理平台", "link": "https://gab.122.gov.cn/"}
+{"title": "ACL4SSR 在线订阅转换", "link": "https://acl4ssr-sub.github.io/"}
+{"title": "飞桨AI Studio—AI 学习与实训社区", "link": "https://aistudio.baidu.com/index"}
+{"title": "", "link": ""}
+`.split("\n")
+// 一行一个json
